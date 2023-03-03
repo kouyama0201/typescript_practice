5179ce4003cf260b663a61110e3282415e6087b6
kouyama0201 <kami19900201@gmail.com>
2023-03-03 17:07:20 +0900

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+// 文字列型
+var title: string = 'Angular';
+title = 'Angular!';
+
+// 数値型
+var n: number = 9;
+
+// 真偽値型
+var isOpen: boolean = true;
+
+// 配列型
+var keywords: (string | number)[] = ['name', 'email', 'body', 0];
+
+// ダブル型
+var payment: [number, string, number] = [1, 'apple', 0];
+
+// オブジェクト型
+var object: { [key: string]: string } = {
+  name: 'Last First',
+  id: 'test',
+};
+var post: object = [{ id: 12, content: 'lorem ipsum' }];
