c83e51889361e0fc3fd4da1ae0f467f8c206f004
kouyama0201 <kami19900201@gmail.com>
2023-03-05 19:08:47 +0900

    22. 関数の基本を理解しよう

--- index.ts
+++ index.ts
@@ -175,3 +175,17 @@ const { id, name } = user.accountInfo.data;
 function test({ a, b, c }) {
 
 }
+
+// 関数
+function greet(name: string = 'tk'): void {
+  console.log(`Hello world! ${name}`);
+}
+greet('Taro');
+
+// 可変長引数
+function sum(...values: number[]): number {
+  return values.reduce(function(prev, current) {
+    return prev + current;
+  });
+}
+console.log(sum(1, 3, 5, 9));
